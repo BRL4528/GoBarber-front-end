2ecf973b71ab5f724ceabfdd511dac4c19743a44
BRL4528 <blgc.sgo@hotmail.com>
2020-07-08 22:20:44 -0400

    add extensios toast

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import SingIn from './pages/Signin';
 import SingUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
+import { ToastProvider } from './hooks/ToastContext';
+
 const App: React.FC = () => (
   <>
-    <SingIn />
+    <AuthProvider>
+      <SingIn />
+    </AuthProvider>
+
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
