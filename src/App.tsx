753d3a66b9305e2a49e1fb2ad2d18cd7f28efc6b
BRL4528 <blgc.sgo@hotmail.com>
2020-07-14 04:41:04 -0400

    continuando back end gobarber :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SingIn from './pages/Signin';
-import SingUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import ToastContainer from './components/ToastContainer';
-import { AuthProvider } from './hooks/AuthContext';
-import { ToastProvider } from './hooks/ToastContext';
+import AppProvider from './hooks';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
-    <AuthProvider>
-      <SingIn />
-    </AuthProvider>
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
 
-    <ToastContainer />
     <GlobalStyle />
-  </>
+  </Router>
 );
 export default App;
